dc2681eebf9358d3ad64265ff6b5e66964d23b69
susanamb <48146503+susanamb@users.noreply.github.com>
2021-02-28 11:00:49 -0800

    Mad Libs Game

---
+++ madlib.py
@@ -0,0 +1,6 @@
+
+holiday = input("Escribe una festividad: ")
+clothes = input("Escribe una ropa: ")
+place   = input("Escribe un lugar: ")
+
+print(f"No puedo creer que ya es {holiday}! \n No puedo esperar para usar mis {clothes} y visitar cada {place} cerca.")
